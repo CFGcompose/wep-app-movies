4ad7e26bb46ae8d57b14563a6445b8c505decde0
Christoph Güttner <78804130+CFGcompose@users.noreply.github.com>
2023-02-24 14:15:45 +0100

    Add files via upload

--- application.py
+++ application.py
@@ -1,28 +1,36 @@
-from flask import Flask
-from simple_recommender import get_recommendations
-from flask import render_template
-from flask import request
-
+from flask import Flask,render_template, request
+from recommender import recommend_random
+from utils import movies
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html', title='Hello, World!')
-    
-@app.route('/recommender')
-def recommender():
-    html_form_data = dict(request.args)
-    # a python dictionary consisting of
-    # "name"-value pairs from the HTML form!
+def hello():
+    """
+    Returns:
+        hello is printed out
+    """
+    return render_template('index.html',name ='Binomial Baharat',movies = movies.title.to_list())
 
-    recs = get_recommendations()
-    # at this point, we would then pass this
-    #information as an argument into our recommender function.
+@app.route('/recommend')
+def recommendations():
+    if request.args['algo']=='Random':
+        recs = recommend_random() 
+        print(request.args)
 
-    print(html_form_data)
+        titles = request.args.getlist('title')
+        ratings = request.args.getlist('Ratings')
+        user_input = dict(zip(titles,ratings))
 
-    return render_template('recommendations.html',
-                            movies = recs)
-    
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+        print(user_input)
+
+
+        for keys in user_input:
+            user_input[keys] = int(user_input[keys])
+
+
+        return render_template('recommend.html',recs =recs)
+    else:
+        return f"Function not defined"
+
+if __name__=='__main__':
+    app.run(debug=True,port=5000)
